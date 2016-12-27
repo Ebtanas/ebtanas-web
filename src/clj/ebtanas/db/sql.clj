86658c9b0602a09dd50eae1ce36f01dc64827720
Nadiar AS <codxse@gmail.com>
2016-12-27 07:53:49 +0700

    .gitignore is now working

--- src/clj/ebtanas/db/sql.clj
+++
@@ -1,4 +0,0 @@
-(ns ebtanas.db.sql
-  (require [hugsql.core :as hugsql]))
-
-(hugsql/def-db-fns "ebtanas/db/sql/user_queries.sql")
--- src/clj/ebtanas/handlers/auth.clj
+++
@@ -1,9 +0,0 @@
-(ns ebtanas.handlers.auth
-  (require [ebtanas.db.connection :refer [db-con]]
-           [ebtanas.db.sql :as sql]
-           [ring.util.http-response :as response]
-           [buddy.hashers :as hashers]
-           [clojure.tools.logging :as log]))
-
-
-
